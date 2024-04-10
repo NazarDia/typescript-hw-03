858f3bd18aa7915616f76dfcd61f0e8148e764bf
Nazar Dia <nazar.dia@gmail.com>
2024-04-10 20:15:01 +0200

    hw3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,56 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
-const house = new MyHouse(key);
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} get into the house.`);
+    } else {
+      console.log("The door is closed. You can't come in.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Wrong key. You can't open the door.");
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse();
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
+house.openDoor(key);
 house.comeIn(person);
-
 
 export {};
